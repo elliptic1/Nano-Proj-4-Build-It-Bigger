b81e404b1731170501fb2e45e0810b0d597d3fcb
Todd Smith <mail@toddbsmith.com>
2016-03-14 06:19:34 -0400

    Added Google Cloud module, free and paid versions, signing config and keystore

---
+++ jokedisplaylib/src/androidTest/java/proj4/nano/tbse/com/jokedisplaylib/ApplicationTest.java
@@ -0,0 +1,13 @@
+package proj4.nano.tbse.com.jokedisplaylib;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ jokedisplaylib/src/main/java/proj4/nano/tbse/com/jokedisplaylib/JokeViewActivity.java
@@ -0,0 +1,13 @@
+package proj4.nano.tbse.com.jokedisplaylib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JokeViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke_view);
+    }
+}

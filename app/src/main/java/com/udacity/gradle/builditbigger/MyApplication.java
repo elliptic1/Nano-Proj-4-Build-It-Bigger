6f27b27fac7333cd842f72bf3c60a5a1eb065fde
Todd Smith <mail@toddbsmith.com>
2016-03-14 05:28:41 -0400

    Added Google Cloud module, updated SDK, added multidex

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/MyApplication.java
@@ -0,0 +1,9 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by todd on 3/14/16.
+ */
+public class MyApplication extends MultiDexApplication {
+}
